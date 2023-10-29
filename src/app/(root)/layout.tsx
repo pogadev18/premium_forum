96864ecc6086bc315a6bae21703e81130fc5080d
pogadev <pogadev18@gmail.com>
2023-10-29 14:17:21 +0200

    added sidebar navigation

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,19 +1,21 @@
 import { PropsWithChildren } from 'react';
 
 import Navbar from '@/components/shared/navbar/Navbar';
+import NavContent from '@/components/shared/navbar/NavContent';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSidebar
+        <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+          <NavContent /> {/*  Left Sidebar */}
+        </section>
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <section>RightSidebar</section>
       </div>
-      Toaster
     </main>
   );
 };
